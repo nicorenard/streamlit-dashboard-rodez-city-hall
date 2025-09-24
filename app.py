8aa16c0ba976408243f0224ca540c7cf5c25653e
Nicolas Renard <nam.renard.pro@gmail.com>
2025-09-24 12:05:19 +0200

    feat : add wide option in content

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ ad_analysis = st.Page(
     icon=":material/search_insights:",
 )
 game = st.Page("src/pages/game.py", title="Let's play !", icon=":material/joystick:")
+st.set_page_config(layout="wide")
 
 pg = st.navigation(
     pages=[home, birth, death, wedding, exploration, ad_analysis, game], position="top"
