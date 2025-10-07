dd3d8bbe2dd15a50d5aba7afa9b9e081f2b6693f
Nicolas Renard <nam.renard.pro@gmail.com>
2025-10-07 16:37:19 +0200

    feat : ajout des variables de setting

---
+++ src/config/__init__.py
@@ -0,0 +1,1 @@
+from .setting import PAGE_CONFIG, VERSION
---
+++ src/config/setting.py
@@ -0,0 +1,13 @@
+VERSION = "1.0.0"
+
+PAGE_CONFIG = {
+    "page_title": "Rodez : histoire en données",
+    "page_icon": "📊",
+    "layout": "wide",
+    "initial_sidebar_state": "expanded",
+    "menu_items": {
+        "Get help": "https://github.com/nicorenard/streamlit-dashboard-rodez-city-hall/blob/main/README.md",
+        "Report a bug": "https://github.com/nicorenard/streamlit-dashboard-rodez-city-hall/issues",
+        "About": "v1.0.0 - Créée par Nicolas Renard",
+    },
+}
