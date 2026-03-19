c425b05f0d1010efebdc04517eadac51a4d0f159
Nicolas Renard <nam.renard.pro@gmail.com>
2026-03-19 19:44:51 +0100

    fix : correct docstring

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,10 +6,10 @@ def render_footer():
     st.markdown("""---""")
     st.html(f"""
     <div class="footer-bottom">
-        <p>Auteur : Nicolas Renard | Version application: {VERSION} | <a href="https://github.com/nicorenard/streamlit-dashboard-rodez-city-hall" target="_blank">Source projet - Github</a></p>
+        <p>Auteur : Nicolas Renard | Version de l\'application: {VERSION} | <a href="https://github.com/nicorenard/streamlit-dashboard-rodez-city-hall" target="_blank">Source projet - Github</a></p>
         <p><a href="https://www.data.gouv.fr/datasets/population-mariages-deces-naissances-1/" target="_blank">Source des datasets</a> | Dernière mise à jour : 17-10-2016</p>
     </div>
     <div class="footer-bottom">
-        © 2025 - Tous droits réservés - Nicolas Renard
+        © 2026 - Tous droits réservés - Nicolas Renard
     </div>
     """)
